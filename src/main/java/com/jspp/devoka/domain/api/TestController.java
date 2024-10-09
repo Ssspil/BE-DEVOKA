8297316e8fc34bb78bc01c38c2e4c5b7d2861d48
Ssspil <rius0918@gmail.com>
2024-10-09 17:34:09 +0900

    테스트 API 추가

--- src/main/java/com/jspp/devoka/domain/api/TestController.java
+++ src/main/java/com/jspp/devoka/domain/api/TestController.java
@@ -1,17 +1,19 @@
 package com.jspp.devoka.domain.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String test(){
         return "test";
     }
 
-    @RequestMapping("/status")
+    @PostMapping("/status")
     public String status(){
         return "very good";
     }
