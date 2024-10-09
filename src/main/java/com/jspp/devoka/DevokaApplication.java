41510905b5404477f05f461980a957319ece7cd8
Ssspil <rius0918@gmail.com>
2024-10-09 16:54:40 +0900

    init

---
+++ src/main/java/com/jspp/devoka/DevokaApplication.java
@@ -0,0 +1,13 @@
+package com.jspp.devoka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevokaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevokaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jspp/devoka/DevokaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspp.devoka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevokaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
